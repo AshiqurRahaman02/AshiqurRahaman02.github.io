924610c2c04e268084187bf66b7f7d100ed4e1ae
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-07-25 15:14:22 +0530

    fixing

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ function scrollToContact() {
 	});
 }
 
+function openResumeInNewTab() {
+
+	// Get the resume link
+	const resumeLink = "./Ashiqur-Rahaman -Resume.pdf"
+
+	// Open the resume in a new tab or window
+	window.open(resumeLink, "_blank");
+}
+
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
@@ -57,42 +66,40 @@ const text = document.querySelector("#text");
 sendEmail.addEventListener("click", (e) => {
 	e.preventDefault();
 
-  let details={
-    from:email.value,
-    to: "ashiqur999999@gmail.com",
-    subject: "Email from Portfolio",
-    text: `${name.value} \n ${number.value} \n ${text.value} \n from ${email.value}` 
-  }
-  // console.log(details);
-  fetch("https://tiny-lime-jay-coat.cyclic.app/send-email", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(details),
-  })
-  .then(res=>res.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+	let details = {
+		from: email.value,
+		to: "ashiqur999999@gmail.com",
+		subject: "Email from Portfolio",
+		text: `${name.value} \n ${number.value} \n ${text.value} \n from ${email.value}`,
+	};
+	// console.log(details);
+	fetch("https://tiny-lime-jay-coat.cyclic.app/send-email", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(details),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 const slider = document.querySelector("#slider");
-const tags = document.querySelector("#sTags")
-slider.addEventListener("click",() => {
-  const computedStyle = window.getComputedStyle(slider);
-  const rotation = computedStyle.getPropertyValue('rotate');
+const tags = document.querySelector("#sTags");
+slider.addEventListener("click", () => {
+	const computedStyle = window.getComputedStyle(slider);
+	const rotation = computedStyle.getPropertyValue("rotate");
 
-  
-
-  if(rotation == "180deg"){
-    slider.style.rotate = "90deg"
-    tags.style.visibility = "visible"
-  }else{
-    slider.style.rotate = "180deg"
-    tags.style.visibility = "hidden"
-  }
-})
+	if (rotation == "180deg") {
+		slider.style.rotate = "90deg";
+		tags.style.visibility = "visible";
+	} else {
+		slider.style.rotate = "180deg";
+		tags.style.visibility = "hidden";
+	}
+});
